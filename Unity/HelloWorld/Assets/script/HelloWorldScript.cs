702071bab2beff1e42e3ac9bacc6ab5e149ac1c5
V-Ananya <76559512+V-Ananya@users.noreply.github.com>
2021-06-19 18:36:44 +0530

    Add files via upload

---
+++ Unity/HelloWorld/Assets/script/HelloWorldScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HelloWorldScript : MonoBehaviour
+{
+    public InputField nameField;
+    public GameObject popupPanel;
+    public Text messageText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hidePopupPanel ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void showPopupPanel()
+    {
+        popupPanel.SetActive(true);
+    }
+    public void hidePopupPanel()
+    {
+        popupPanel.SetActive(false);
+    }
+    public void Hello()
+    {
+        messageText.text = "Hello " + nameField.text + "!";
+        showPopupPanel();
+    }
+}
